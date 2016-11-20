955fb27e339d4fc8fce90dbee57a38e6e81474bc
Tal Goldfus <talgoldfus@gmail.com>
2016-11-19 23:51:28 -0500

    Add AddTaskButton Modal

--- GithubProjectManagerReact/src/components/AddTaskButton.js
+++ GithubProjectManagerReact/src/components/AddTaskButton.js
@@ -1,14 +1,59 @@
-import React from 'react';
+import React,{Component} from 'react';
 import FloatingActionButton from 'material-ui/FloatingActionButton';
 import ContentAdd from 'material-ui/svg-icons/content/add';
-
-const AddTaskButton = () => (
-  <div>
-    <FloatingActionButton secondary={true}>
-      <ContentAdd />
-    </FloatingActionButton>
-  </div>
-);
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
 
 
-export default AddTaskButton
+class AddTaskButton extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+        open: false,
+      };
+  }
+
+  handleOpen(){
+    this.setState({open: true});
+  };
+
+  handleClose(){
+    this.setState({open: false});
+  };
+
+
+  render(){
+
+    const actions = [
+      <FlatButton
+        label="Submit"
+        primary={true}
+        keyboardFocused={true}
+        onTouchTap={this.handleClose.bind(this)}
+      />
+    ];
+
+    return(
+
+      <div>
+        <FloatingActionButton
+          secondary={true}
+          onTouchTap={this.handleOpen.bind(this)}
+          >
+          <ContentAdd />
+        </FloatingActionButton>
+      </div>
+
+        <Dialog
+          actions={actions}
+          modal={false}
+          open={this.state.open}
+        >
+        </Dialog>
+      </div>
+    )
+  }
+};
+
+export default AddTaskButton;
